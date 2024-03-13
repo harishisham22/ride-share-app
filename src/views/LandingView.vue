a0e4b855acab12930120a290704627c5e681435b
harishisham22 <you@example.comharis.hisham22@gmail.com>
2024-03-13 14:10:27 +0800

    first commit

---
+++ src/views/LandingView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="pt-16">
+        <h1 class="text-3xl font-semibold mb-4">Uwuber</h1>
+        <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+            <div class="bg-white px-4 py-5 sm:p-6">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <button
+                        class="rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white">
+                        Start Driving
+                    </button>
+                    <button
+                        @click="handleFindARide"
+                        class="rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white">
+                        Find A Ride
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+
+const router = useRouter()
+const handleFindARide = () => {
+    router.push({
+        name: 'location'
+    })
+}
+</script>
---
+++ src/views/LocationView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="pt-16">
+        <h1 class="text-3xl font-semibold mb-4">Where are we going?</h1>
+        <form action="#">
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <GMapAutocomplete 
+                            placeholder="My destination"
+                            @place_changed="handleLocationChanged"
+                            class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm">
+                        </GMapAutocomplete>
+                    </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button 
+                        type="submit" 
+                        class="inline-flex justify-center rounded-md border border-transparent bg-black text-white py-2 px-2">
+                        Continue
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { useLocationStore } from '@/stores/location';
+
+const location = useLocationStore();
+const handleLocationChanged = (e) => {
+    console.log('handleLocationChanged', e)
+}
+</script>
